81b5b1a8914c0d43b92e1acf33bf74d77ce41f81
korobochko-coder <korobochko.a@gmail.com>
2023-02-08 16:01:22 -0500

    SearchEngine

---
+++ src/SearchEngine.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SearchEngine() {
+  return (
+    <div className="SearchEngine">
+      <form>
+        <input type="search" placeholder="Type your city" />
+      </form>
+    </div>
+  );
+}
