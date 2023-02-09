d3365e91e905220ff9eeee6f8554ec1d034c9626
korobochko-coder <korobochko.a@gmail.com>
2023-02-09 12:23:46 -0500

    AppContainer

--- src/index.js
+++ src/index.js
@@ -10,23 +10,25 @@ import "bootstrap/dist/css/bootstrap.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="container">
-      <div className="row">
-        <div className="col-6">
-          <SearchEngine />
-        </div>
-        <div className="col-6">
-          <LocationButton />
+    <div className="AppContainer">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <SearchEngine />
+          </div>
+          <div className="col-6">
+            <LocationButton />
+          </div>
         </div>
       </div>
+      <CurrentWeather
+        day="Monday"
+        time="12:50"
+        description="Sunny"
+        humidity="10"
+        wind="8"
+      />
     </div>
-    <CurrentWeather
-      day="Monday"
-      time="12:50"
-      description="Sunny"
-      humidity="10"
-      wind="8"
-    />
   </React.StrictMode>
 );
 
